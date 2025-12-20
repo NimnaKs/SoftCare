de0569c0f667921f1117e165f9c88bb45ad01256
NimnaKs <nimnakaveesha123456@gmail.com>
2025-12-20 08:29:44 +0530

    Init the project

---
+++ src/main/java/me/nimnakse/water_management/WaterManagementApplication.java
@@ -0,0 +1,13 @@
+package me.nimnakse.water_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaterManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaterManagementApplication.class, args);
+	}
+
+}
