af0a88c967bf8da217c187fb764ea4523555408d
Nimna Kaveesha Sekara <125631747+NimnaKs@users.noreply.github.com>
2025-12-21 09:11:45 +0530

    Fix role deletedAt support

--- src/main/java/me/nimnakse/water_management/roles/entity/Role.java
+++ src/main/java/me/nimnakse/water_management/roles/entity/Role.java
@@ -4,22 +4,16 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
+import me.nimnakse.water_management.common.entity.BaseEntity;
 
 @Getter
 @Setter
 @Entity
 @Table(name = "roles")
-public class Role {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
+public class Role extends BaseEntity {
     @Column(nullable = false, length = 100)
     private String name;
 
