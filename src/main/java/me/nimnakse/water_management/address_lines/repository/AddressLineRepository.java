cf1ecf8fddb27bb7fee81a9b9a9ac848f9b17ab3
Nimna Kaveesha Sekara <125631747+NimnaKs@users.noreply.github.com>
2025-12-22 19:37:03 +0530

    Add CRUD operations for address lines

--- src/main/java/me/nimnakse/water_management/address_lines/repository/AddressLineRepository.java
+++ src/main/java/me/nimnakse/water_management/address_lines/repository/AddressLineRepository.java
@@ -3,8 +3,6 @@ package me.nimnakse.water_management.address_lines.repository;
 import java.util.List;
 import java.util.Optional;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import me.nimnakse.water_management.address_lines.entity.AddressLine;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -18,6 +16,16 @@ public interface AddressLineRepository extends JpaRepository<AddressLine, Long> 
             Long orgUnitId
     );
 
+    boolean existsByLevelAndNameIgnoreCaseAndParentLine1IdAndParentLine2IdAndParentLine3IdAndOrgUnitIdAndIdNot(
+            Integer level,
+            String name,
+            Long parentLine1Id,
+            Long parentLine2Id,
+            Long parentLine3Id,
+            Long orgUnitId,
+            Long id
+    );
+
     Optional<AddressLine> findTopByLevelAndParentLine1IdAndParentLine2IdAndParentLine3IdAndOrgUnitIdOrderByInternalCodeDesc(
             Integer level,
             Long parentLine1Id,
@@ -27,4 +35,8 @@ public interface AddressLineRepository extends JpaRepository<AddressLine, Long> 
     );
 
     List<AddressLine> findByNameContainingIgnoreCase(String name);
+
+    boolean existsByParentLine1IdOrParentLine2IdOrParentLine3Id(Long parentLine1Id,
+                                                                Long parentLine2Id,
+                                                                Long parentLine3Id);
 }
--- src/main/java/me/nimnakse/water_management/address_lines/service/AddressLineService.java
+++ src/main/java/me/nimnakse/water_management/address_lines/service/AddressLineService.java
@@ -2,6 +2,7 @@ package me.nimnakse.water_management.address_lines.service;
 
 import java.util.List;
 import me.nimnakse.water_management.address_lines.dto.request.AddressLineCreateReq;
+import me.nimnakse.water_management.address_lines.dto.request.AddressLineUpdateReq;
 import me.nimnakse.water_management.address_lines.dto.response.AddressLineHierarchyRes;
 import me.nimnakse.water_management.address_lines.dto.response.AddressLineRes;
 
@@ -10,5 +11,11 @@ public interface AddressLineService {
 
     List<AddressLineRes> search(String query);
 
+    AddressLineRes getById(Long id);
+
     AddressLineHierarchyRes getHierarchy(Long id);
+
+    AddressLineRes update(Long id, AddressLineUpdateReq request);
+
+    void delete(Long id);
 }
