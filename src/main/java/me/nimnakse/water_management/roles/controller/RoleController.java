6534ccb5e3aed90774c710600781ae0c73f8e260
Nimna Kaveesha <nimnakaveesha123456@gmail.com>
2025-12-26 22:36:50 +0530

    add cross-origin for role

--- src/main/java/me/nimnakse/water_management/roles/controller/RoleController.java
+++ src/main/java/me/nimnakse/water_management/roles/controller/RoleController.java
@@ -10,18 +10,12 @@ import me.nimnakse.water_management.roles.dto.RoleRes;
 import me.nimnakse.water_management.roles.dto.RoleUpdateReq;
 import me.nimnakse.water_management.roles.service.RoleService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/roles")
 @Tag(name = "Roles", description = "Role management operations")
+@CrossOrigin
 public class RoleController {
     private final RoleService roleService;
 
