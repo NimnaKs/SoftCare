a2809adcf9873d5ee1eb7110ed4bf0cfc072d0bb
Nimna Kaveesha <nimnakaveesha123456@gmail.com>
2025-12-27 00:13:23 +0530

    add cross-origin for users

--- src/main/java/me/nimnakse/water_management/users/controller/UserController.java
+++ src/main/java/me/nimnakse/water_management/users/controller/UserController.java
@@ -10,18 +10,12 @@ import me.nimnakse.water_management.users.dto.request.UserUpdateReq;
 import me.nimnakse.water_management.users.dto.response.UserRes;
 import me.nimnakse.water_management.users.service.UserService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
 @Tag(name = "Users", description = "User management operations")
+@CrossOrigin
 public class UserController {
     private final UserService userService;
 
