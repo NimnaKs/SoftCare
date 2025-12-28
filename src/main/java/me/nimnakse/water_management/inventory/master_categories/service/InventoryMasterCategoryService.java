f44d1029ba93f7b38c1d14783ccbad1835168d95
Nimna Kaveesha Sekara <125631747+NimnaKs@users.noreply.github.com>
2025-12-28 11:43:31 +0530

    Add paginated inventory master category lookups by level

--- src/main/java/me/nimnakse/water_management/inventory/master_categories/service/InventoryMasterCategoryService.java
+++ src/main/java/me/nimnakse/water_management/inventory/master_categories/service/InventoryMasterCategoryService.java
@@ -5,6 +5,7 @@ import me.nimnakse.water_management.inventory.master_categories.dto.request.Inve
 import me.nimnakse.water_management.inventory.master_categories.dto.request.InventoryMasterCategoryUpdateReq;
 import me.nimnakse.water_management.inventory.master_categories.dto.response.InventoryMasterCategoryRes;
 import me.nimnakse.water_management.inventory.master_categories.dto.response.InventoryMasterCategoryTreeRes;
+import me.nimnakse.water_management.common.api.PageResponse;
 
 public interface InventoryMasterCategoryService {
     InventoryMasterCategoryRes create(InventoryMasterCategoryCreateReq request);
@@ -16,6 +17,12 @@ public interface InventoryMasterCategoryService {
     List<InventoryMasterCategoryRes> list();
 
     List<InventoryMasterCategoryTreeRes> getTree();
+
+    PageResponse<InventoryMasterCategoryRes> getLevelOneCategories(int page, int size);
+
+    PageResponse<InventoryMasterCategoryRes> getLevelTwoCategories(Long levelOneCategoryId, int page, int size);
+
+    PageResponse<InventoryMasterCategoryRes> getLevelThreeCategories(Long levelTwoCategoryId, int page, int size);
 
     void delete(Long id);
 }
