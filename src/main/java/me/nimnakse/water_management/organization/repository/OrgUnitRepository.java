b91915fbc5c2acb81d06d269ebdc2c1b83ec93a3
Nimna Kaveesha <nimnakaveesha123456@gmail.com>
2025-12-30 11:18:59 +0530

    add organization unit code for organization unit  for tree

--- src/main/java/me/nimnakse/water_management/organization/repository/OrgUnitRepository.java
+++ src/main/java/me/nimnakse/water_management/organization/repository/OrgUnitRepository.java
@@ -8,6 +8,8 @@ import me.nimnakse.water_management.organization.entity.OrgUnitLevel;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface OrgUnitRepository extends JpaRepository<OrgUnit, Long> {
     List<OrgUnit> findByWaterProjectId(Long waterProjectId);
@@ -20,5 +22,6 @@ public interface OrgUnitRepository extends JpaRepository<OrgUnit, Long> {
 
     Optional<OrgUnit> findTopByLevelOrderByOrganizationCodeDesc(OrgUnitLevel level);
 
-    String findMaxOrganizationCodeByLevel(OrgUnitLevel orgUnitLevel);
+    @Query("select max(o.organizationCode) from OrgUnit o where o.level = :level")
+    String findMaxOrganizationCodeByLevel(@Param("level") OrgUnitLevel level);
 }
