e010ad8f2ae4f761fb318682b31c06c92ccda0b7
Nimna Kaveesha <nimnakaveesha123456@gmail.com>
2026-01-03 21:50:59 +0530

    fix bugs in imports

--- src/main/java/me/nimnakse/water_management/inventory/templates/controller/InventoryTemplateController.java
+++ src/main/java/me/nimnakse/water_management/inventory/templates/controller/InventoryTemplateController.java
@@ -22,6 +22,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/inventory-templates")
 @Tag(name = "Inventory Templates", description = "Manage inventory templates derived from master categories")
