56c13e2c76d8ac7de7fe093356908887924fe7a5
Nimna Kaveesha <nimnakaveesha123456@gmail.com>
2026-01-24 15:34:27 +0530

    Fix : fix account code error .

--- src/main/java/me/nimnakse/water_management/expenses/main_categories/repository/ExpenseMainCategoryRepository.java
+++ src/main/java/me/nimnakse/water_management/expenses/main_categories/repository/ExpenseMainCategoryRepository.java
@@ -6,16 +6,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface ExpenseMainCategoryRepository extends JpaRepository<ExpenseMainCategory, Long> {
-    boolean existsByCode(Integer code);
-
-    boolean existsByCodeAndIdNot(Integer code, Long id);
 
     boolean existsByNameIgnoreCase(String name);
 
     boolean existsByNameIgnoreCaseAndIdNot(String name, Long id);
 
     Optional<ExpenseMainCategory> findByNameIgnoreCase(String name);
-
-    @Query("select max(category.code) from ExpenseMainCategory category")
-    Integer findMaxCode();
 }
