485e11c39c5fa39b36157a2515dc846f22481c11
Nimna Kaveesha <nimnakaveesha123456@gmail.com>
2026-02-01 08:05:29 +0530

    feat: implement Spring Security configuration with JWT authentication and role-based access control.

--- src/main/java/me/nimnakse/water_management/security/SecurityConfig.java
+++ src/main/java/me/nimnakse/water_management/security/SecurityConfig.java
@@ -18,104 +18,109 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration
 @EnableMethodSecurity
 public class SecurityConfig {
-    private final CustomUserDetailsService userDetailsService;
-    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+        private final CustomUserDetailsService userDetailsService;
+        private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
-    public SecurityConfig(CustomUserDetailsService userDetailsService,
-                          JwtAuthenticationFilter jwtAuthenticationFilter) {
-        this.userDetailsService = userDetailsService;
-        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
-    }
+        public SecurityConfig(CustomUserDetailsService userDetailsService,
+                        JwtAuthenticationFilter jwtAuthenticationFilter) {
+                this.userDetailsService = userDetailsService;
+                this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+        }
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(AbstractHttpConfigurer::disable)
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(
-                                HttpMethod.OPTIONS, "/**"
-                        ).permitAll()
-                        .requestMatchers(
-                                "/auth/**",
-                                "/health/**",
-                                "/v3/api-docs/**",
-                                "/swagger-ui/**"
-                        ).permitAll()
-                        .requestMatchers(HttpMethod.GET,
-                                "/revenue-main-categories/**",
-                                "/revenue-accounts/**",
-                                "/fixed-asset-master-categories/**",
-                                "/fixed-asset-templates/**",
-                                "/inventory-master-categories/**",
-                                "/inventory-templates/**",
-                                "/expense-main-categories/**",
-                                "/expense-accounts/**",
-                                "/liability-main-categories/**",
-                                "/liability-accounts/**",
-                                "/org-units/**"
-                        ).hasAnyAuthority("APP_SCOPE_ADMIN_PORTAL", "APP_SCOPE_BRANCH_APP")
-                        .requestMatchers(
-                                HttpMethod.POST,
-                                "/inventory-templates/import",
-                                "/fixed-asset-templates/import"
-                        ).hasAnyAuthority("APP_SCOPE_ADMIN_PORTAL", "APP_SCOPE_BRANCH_APP")
-                        .requestMatchers(
-                                HttpMethod.GET,
-                                "/fixed-asset-templates/**",
-                                "/inventory-templates/**"
-                        ).hasAnyAuthority("APP_SCOPE_ADMIN_PORTAL", "APP_SCOPE_BRANCH_APP")
-                        .requestMatchers(
-                                "/members/**",
-                                "/employees/**",
-                                "/address-lines/**",
-                                "/connections/**",
-                                "/inventory-consumptions/**",
-                                "/fixed-asset-consumptions/**",
-                                "/fixed-asset-bin-cards/**",
-                                "/inventory-consumptions/**",
-                                "/inventory-bin-cards/**",
-                                "/monetary-transactions/**",
-                                "/purchase-vouchers/**",
-                                "/purchase-voucher-drafts/**"
-                        ).hasAuthority("APP_SCOPE_BRANCH_APP")
-                        .requestMatchers(
-                                "/roles/**",
-                                "/users/**",
-                                "/water-projects/**",
-                                "/org-units/**",
-                                "/organizations/**",
-                                "/revenue-main-categories/**",
-                                "/revenue-accounts/**",
-                                "/fixed-asset-master-categories/**",
-                                "/fixed-asset-templates/**",
-                                "/fixed-asset-consumptions/**",
-                                "/fixed-asset-bin-cards/**",
-                                "/inventory-master-categories/**",
-                                "/inventory-templates/**",
-                                "/inventory-consumptions/**",
-                                "/inventory-bin-cards/**",
-                                "/expense-main-categories/**",
-                                "/expense-accounts/**",
-                                "/liability-main-categories/**",
-                                "/liability-accounts/**"
-                        ).hasAuthority("APP_SCOPE_ADMIN_PORTAL")
-                        .anyRequest().authenticated()
-                )
-                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        @Bean
+        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+                http
+                                .csrf(AbstractHttpConfigurer::disable)
+                                .sessionManagement(session -> session
+                                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                                .authorizeHttpRequests(auth -> auth
+                                                .requestMatchers(
+                                                                HttpMethod.OPTIONS, "/**")
+                                                .permitAll()
+                                                .requestMatchers(
+                                                                "/auth/**",
+                                                                "/health/**",
+                                                                "/v3/api-docs/**",
+                                                                "/swagger-ui/**")
+                                                .permitAll()
+                                                .requestMatchers(HttpMethod.GET,
+                                                                "/revenue-main-categories/**",
+                                                                "/revenue-accounts/**",
+                                                                "/fixed-asset-master-categories/**",
+                                                                "/fixed-asset-templates/**",
+                                                                "/inventory-master-categories/**",
+                                                                "/inventory-templates/**",
+                                                                "/expense-main-categories/**",
+                                                                "/expense-accounts/**",
+                                                                "/liability-main-categories/**",
+                                                                "/liability-accounts/**",
+                                                                "/org-units/**")
+                                                .hasAnyAuthority("APP_SCOPE_ADMIN_PORTAL", "APP_SCOPE_BRANCH_APP")
+                                                .requestMatchers(
+                                                                HttpMethod.POST,
+                                                                "/inventory-templates/import",
+                                                                "/fixed-asset-templates/import")
+                                                .hasAnyAuthority("APP_SCOPE_ADMIN_PORTAL", "APP_SCOPE_BRANCH_APP")
+                                                .requestMatchers(
+                                                                HttpMethod.GET,
+                                                                "/fixed-asset-templates/**",
+                                                                "/inventory-templates/**")
+                                                .hasAnyAuthority("APP_SCOPE_ADMIN_PORTAL", "APP_SCOPE_BRANCH_APP")
+                                                .requestMatchers(
+                                                                "/members/**",
+                                                                "/employees/**",
+                                                                "/address-lines/**",
+                                                                "/connections/**",
+                                                                "/inventory-consumptions/**",
+                                                                "/fixed-asset-consumptions/**",
+                                                                "/fixed-asset-bin-cards/**",
+                                                                "/inventory-consumptions/**",
+                                                                "/inventory-bin-cards/**",
+                                                                "/monetary-transactions/**",
+                                                                "/purchase-vouchers/**",
+                                                                "/purchase-voucher-drafts/**",
+                                                                "/societies/**",
+                                                                "/gn-divisions/**",
+                                                                "/valves/**",
+                                                                "/billing-zones/**",
+                                                                "/clusters/**")
+                                                .hasAuthority("APP_SCOPE_BRANCH_APP")
+                                                .requestMatchers(
+                                                                "/roles/**",
+                                                                "/users/**",
+                                                                "/water-projects/**",
+                                                                "/org-units/**",
+                                                                "/organizations/**",
+                                                                "/revenue-main-categories/**",
+                                                                "/revenue-accounts/**",
+                                                                "/fixed-asset-master-categories/**",
+                                                                "/fixed-asset-templates/**",
+                                                                "/fixed-asset-consumptions/**",
+                                                                "/fixed-asset-bin-cards/**",
+                                                                "/inventory-master-categories/**",
+                                                                "/inventory-templates/**",
+                                                                "/inventory-consumptions/**",
+                                                                "/inventory-bin-cards/**",
+                                                                "/expense-main-categories/**",
+                                                                "/expense-accounts/**",
+                                                                "/liability-main-categories/**",
+                                                                "/liability-accounts/**")
+                                                .hasAuthority("APP_SCOPE_ADMIN_PORTAL")
+                                                .anyRequest().authenticated())
+                                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
-        return http.build();
-    }
+                return http.build();
+        }
 
-    @Bean
-    public AuthenticationManager authenticationManager() {
-        DaoAuthenticationProvider provider = new DaoAuthenticationProvider(userDetailsService);
-        provider.setPasswordEncoder(passwordEncoder());
-        return new ProviderManager(provider);
-    }
+        @Bean
+        public AuthenticationManager authenticationManager() {
+                DaoAuthenticationProvider provider = new DaoAuthenticationProvider(userDetailsService);
+                provider.setPasswordEncoder(passwordEncoder());
+                return new ProviderManager(provider);
+        }
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+        @Bean
+        public PasswordEncoder passwordEncoder() {
+                return new BCryptPasswordEncoder();
+        }
 }
